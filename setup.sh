50ca7c2403897cb5b822768dde312d25d2727d95
Occupying-Mars <72188133+Occupyig-Mars@users.noreply.github.com>
2021-08-28 14:14:00 +0530

    new setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
-mkdir -p ~/.streamlit/
-
-echo "\
-[server]\n\
-port = $PORT\n\
-enableCORS = false\n\
-headless = true\n\
-\n\
-" > ~/.streamlit/config.toml
-© 2021 GitHub, Inc.
+    mkdir -p ~/.streamlit/
+    echo “\
+    [general]\n\
+    email = \”<myemail@domain>\”\n\
+    “ > ~/.streamlit/credentials.toml
+    echo “\
+    [server]\n\
+    headless = true\n\
+    enableCORS=false\n\
+    port = $PORT\n\
+    “ > ~/.streamlit/config.toml
