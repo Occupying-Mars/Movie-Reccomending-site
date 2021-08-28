06bf4d3e025834cf5d6b57a4f75d4730e4bd74ac
Occupying-Mars <72188133+Occupyig-Mars@users.noreply.github.com>
2021-08-28 13:53:07 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [server]\n\
-headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
+headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
+© 2021 GitHub, Inc.
