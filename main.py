9eafe2f6a5c35c2b0a82aca63ae971c1d3429609
Occupying-Mars <72188133+Occupyig-Mars@users.noreply.github.com>
2021-09-01 18:44:13 +0530

    final update

--- main.py
+++ main.py
@@ -4,14 +4,10 @@ import requests
 import pandas as pd
 
 def fetch_poster(movie_id):
-    response = requests.get('https://api.themoviedb.org/3/movie/?api_key=8c37dd217ff4493e86ffadf1afe90f3f&language=en-US".format(movie_id)')
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=37635ef860dbd69c611fd91242118b8e&language=en-US'.format(movie_id))
     data = response.json()
-    print(data)
-    st.write(data)
-    full_path = "https://image.tmdb.org/t/p/w500/" + data['poster_path']
-    print(full_path)
-    st.write(data)
-    return full_path
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+     
 
 similarity=pickle.load(open('similarity.pkl','rb'))
 
@@ -26,7 +22,7 @@ def recommend(movie):
         movie_id=movies.iloc[i[0]].movie_id
         recommended_movies.append(movies.iloc[i[0]].title)
         #fetching poster
-        #reccommended_movies_poster.append(fetch_poster(movie_id))
+        reccommended_movies_poster.append(fetch_poster(movie_id))
         #print(reccommended_movies_poster)
         #st.write(reccommended_movies_poster)
     return recommended_movies,reccommended_movies_poster   
@@ -47,20 +43,20 @@ if st.button('Reccommend'):
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.text(names[0])
-        #st.image(posters[0])
+        st.image(posters[0])
     with col2:
         st.text(names[1])
-        #st.image(posters[1])
+        st.image(posters[1])
 
     with col3:
         st.text(names[2])
-        #st.image(posters[2])
+        st.image(posters[2])
     with col4:
         st.text(names[3])
-        #st.image(posters[3])
+        st.image(posters[3])
     with col5:
         st.text(names[4])
-        #st.image(posters[4])
+        st.image(posters[4])
         
         
 
